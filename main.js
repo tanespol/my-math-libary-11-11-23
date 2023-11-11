77055d6d94e57c95bfdd4024c9f844c47bf8a79f
tanespol.pra <tanespol.pra@dome.tu.ac.th>
2023-11-11 11:05:08 +0700

    commit2

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-function sum(a,b) {
-	return a + b
+function sum(a, b) {
+	if (Array.isArray(a)) {
+		let total = 0
+	for (let i = 0; i < a.length; i++) {
+		total += a[i]
 }
-
-module.exports = {
-	sum,
+	return total
+	} else if (typeof a === 'number' && typeof b === 'number') {
+		return a + b
+	}
 }
