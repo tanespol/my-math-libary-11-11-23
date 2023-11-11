adfa40a52be7c9ada6469bd5d1ca1ed0721ca24e
tanespol.pra <tanespol.pra@dome.tu.ac.th>
2023-11-11 13:20:07 +0700

    commit5

--- math.js
+++ math.js
@@ -1,12 +1,13 @@
+// 📄 math.js
+
 function sum(a, b) {
-	if (Array.isArray(a)) {
-		let total = 0
-		for (let i = 0; i < a.length; i++) {
-			total += a[i]
+  if (Array.isArray(a)) {
+    return a.reduce((acc, n) => acc + n, 0)
+  } else if (typeof a === 'number' && typeof b === 'number') {
+    return a + b
+  }
 }
-	return total
-	} else if (typeof a === 'number' && typeof b === 'number') {
-		return a + b
-	}
+
+module.exports = {
+  sum,
 }
-	
