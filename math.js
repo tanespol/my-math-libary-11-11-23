e4ccd388c3030a7d9f6361336a63e4800b630d4f
tanespol.pra <tanespol.pra@dome.tu.ac.th>
2023-11-11 11:27:25 +0700

    commit4

--- math.js
+++ math.js
@@ -1,11 +1,12 @@
 function sum(a, b) {
 	if (Array.isArray(a)) {
 		let total = 0
-	for (let i = 0; i < a.length; i++) {
-		total += a[i]
+		for (let i = 0; i < a.length; i++) {
+			total += a[i]
 }
 	return total
 	} else if (typeof a === 'number' && typeof b === 'number') {
 		return a + b
 	}
 }
+	
--- math.test.js
+++ math.test.js
@@ -1,5 +1,5 @@
-const math = require('./math')
+const math = require('math')
 const assert = require('assert')
 
-assert.strictEqual(math.sum(2,2), 4, 'should sum two numbers')
-assert.strictEqual(math,sum([1,2,3,4,5]),15,'should sum an array')
+assert.strictEqual(math.sum( 2, 2), 4, 'should sum two numbers')
+assert.strictEqual(math.sum([ 1, 2, 3, 4, 5]), 15,'should sum an array') 
